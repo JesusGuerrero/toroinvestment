e56441fb5b3b819d33540432bd6798e02f4f2674
JesusGuerrero <rocha.jesus@hotmail.com>
2018-07-23 06:50:04 -0700

    fixing local dev

--- server/index.js
+++ server/index.js
@@ -1,3 +1,8 @@
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+if( process.env.NODE_ENV === 'development' ) {
+  require('node-env-file')('.env');
+  console.log('Running Development!');
+}
 
 require('mahrio').runServer( process.env, __dirname)
   .then( server => {
